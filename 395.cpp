245de75d0df75ff6c31442d0e8ee7e263f7441a6
zongzi13545329 <13545329@qq.com>
2021-02-28 13:53:56 +0800

    2.27

--- 395.cpp
+++ 395.cpp
@@ -1,7 +1,3 @@
-对于字符串 ss，如果存在某个字符 \textit{ch}ch，它的出现次数大于 00 且小于 kk，则任何包含 \textit{ch}ch 的子串都不可能满足要求。也就是说，我们将字符串按照 \textit{ch}ch 切分成若干段，则满足要求的最长子串一定出现在某个被切分的段内，而不能跨越一个或多个段。因此，可以考虑分治的方式求解本题。
-
-C++JavaJavaScriptGolangC
-
 class Solution {
 public:
     int dfs(const string& s, int l, int r, int k) {
